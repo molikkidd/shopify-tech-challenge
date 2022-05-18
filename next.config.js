32f93bb4504033a327d9686bde4ad83ecb7e55e5
Molikkidd <mzkidd5@gmail.com>
2022-05-17 18:02:09 -0700

    add service worker and test chatbox

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    scope: '/',
+    sw: 'service-worker.js',
+    fallbacks: '/_offline'
+  }
+})
+
+// module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Head from "next/head";
 import React, { useState, useEffect } from "react";
-
+import SubForm from "../components/SubForm";
+import ChatBox from "../components/ChatBox";
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
   const [oprompt, setOprompt] = useState();
   const [listOfResponses, setListOfResponses] = useState(Array);
- 
+  const [newData, setNewData] = useState(null);
+
   async function onSubmit(event) {
 
     event.preventDefault();
@@ -21,71 +22,70 @@ export default function Home() {
     const data = await response.json();
     console.log('data', data)
     // push new data to list of responses
-    // setListOfResponses(data)
     listOfResponses.push(data)
-    setResult(data.result);
-    setOprompt(data.text);
+    // store data in session storage
+    window.localStorage.setItem('one', JSON.stringify(listOfResponses))
     // resets animal input to an empty string
     setAnimalInput("");
   }
-
-  // listOfResponses.push({response:result, prompt: oprompt})
-
-console.log('listOfResponses', listOfResponses)
+  console.log('listOfResponses', listOfResponses)
 
   const responseList = listOfResponses.map((list, index) => {
     console.log('list', list, 'and index', index)
-          return <div className='mt-4 bg-gray-300 rounded-md p-4' index={index}>
-                  <div className="mb-3">
-                    <h1>Prompt:{list.text}  </h1>  
-                  </div>
-                  <div>
-                    <h1>Response: {list.result}</h1>
-                  </div>
-                </div>
+          return <ul className='mt-4 bg-gray-300 rounded-md p-4' i={index}>
+                    <li>
+                      <div className="mb-3">
+                        <h1>Prompt:{list.text}  </h1>  
+                      </div>
+                      <div>
+                        <h1>Response: {list.result}</h1>
+                      </div>
+                    </li>
+                </ul>
 
   })
+  // access data in local storage to use when offline and throughout the local
+  useEffect(() => {
+    const localData = localStorage.getItem('one')
+    console.log('localData', JSON.parse(localData))
+    
+    setNewData(JSON.parse(localData))
+   
+  }, [])
+  
+  
+  
+  // const storedData = newData.map((list, index) => {
+    //   console.log('storedlist', list, 'and storedindex', index)
+    
+    // })
+    if (newData === null ) {
+      return <ChatBox/>
+      // return <SubForm submit={onSubmit} responseList={responseList} setAnimalInput={setAnimalInput} animalInput={animalInput}/>
+    } else {
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    const storedList = newData.map((list, index) => {
+    console.log('list', list, 'and index', index)
+          return <ul className='mt-4 bg-gray-300 rounded-md p-4' i={index}>
+                    <li>
+                      <div className="mb-3">
+                        <h1>Prompt:{list.text}  </h1>  
+                      </div>
+                      <div>
+                        <h1>Response: {list.result}</h1>
+                      </div>
+                    </li>
+                </ul>
 
-      <main className=' h-screen'>
-        <div className="flex justify-center">
-          <div className="xl:w-96">
-          <h3 className="text-xl text-blue-400 mb-3 ">Lets Talk</h3>
+  })
+  console.log('storedList', storedList)
+  return <SubForm submit={onSubmit} 
+          responseList={responseList} 
+          setAnimalInput={setAnimalInput} 
+          animalInput={animalInput}
+          localData={storedList}
+  />
 
-          <form onSubmit={onSubmit}>
-            <input
-              type="text"
-              name="animal"
-              placeholder=" Whats on your mind ..."
-              value={animalInput}
-              onChange={(e) => setAnimalInput(e.target.value)} 
-              className="w-96 h-40 rounded-md mb-3 border-zinc-900 border-2"
-            />
-            <button className="bg-blue-500 px-6 py-2 rounded-md text-white" type="submit">Lets Go</button>
-          </form>
-            <div className='mt-4 bg-gray-300 rounded-md p-4' >
-              <h1 className="text-2xl font-semibold mb-2">Responses</h1>
 
-              <div className="mb-3">
-             <h1 >Prompt: {oprompt}</h1>  
-              </div>
-              <div>
-                <h1>Response: {result}</h1>
-              </div>
-            </div>
-            <div className='mt-4 bg-gray-300 rounded-md p-4'>
-              {responseList}
-
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+} 
 }
